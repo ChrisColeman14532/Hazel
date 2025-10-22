c9164f0677fd12c1176492f1859ef264698d0ce8
ChrisColeman14532 <coleman14532@gmail.com>
2025-10-22 19:40:23 -0400

    Added transforms to Renderer.

--- Hazel/src/Hazel/Renderer/Renderer.h
+++ Hazel/src/Hazel/Renderer/Renderer.h
@@ -12,7 +12,7 @@ namespace Hazel
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader> shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader> shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4 transfrom = glm::mat4(1.0f));
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	private:
@@ -21,6 +21,6 @@ namespace Hazel
 			glm::mat4 ViewProjectionMatrix;
 		};
 
-		static SceneData* m_SceneData;
+		static SceneData* s_SceneData;
 	};
 }
